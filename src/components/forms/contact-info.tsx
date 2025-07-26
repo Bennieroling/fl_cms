f2729e3fbcd21e43bca7376188d1a3953a3dd896
bennieroling <bjsvandijk@gmail.com>
2025-07-26 19:24:49 +0200

    updates

--- src/components/forms/contact-info.tsx
+++ src/components/forms/contact-info.tsx
@@ -1,5 +1,3 @@
-
-
 import { CardContent } from "@/components/ui/card";
 import { Phone, Mail, MapPin } from "lucide-react";
 
@@ -9,7 +7,7 @@ export default function ContactInfo() {
       <div className="space-y-4">
         <div className="flex items-center gap-3">
           <Phone className="w-5 h-5 text-primary" />
-          <span>(555) 123-4567</span>
+          <span>(55 11) 1234-5678</span>
         </div>
         <div className="flex items-center gap-3">
           <Mail className="w-5 h-5 text-primary" />
@@ -17,22 +15,30 @@ export default function ContactInfo() {
         </div>
         <div className="flex items-center gap-3">
           <MapPin className="w-5 h-5 text-primary" />
-          <span>Avenida Corrientes 587, San Nicolás, C1042AAN, Buenos Aires</span>
+      
+          <span>Av. Corrientes 531, C1043AAF,Buenos Aires, Argentina</span>
+        </div>
+        <div className="flex items-center gap-3">
+          <svg xmlns="http://www.w3.org/2000/svg" className="w-5 h-5 text-primary" viewBox="0 0 24 24" fill="none" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round">
+            <circle cx="12" cy="12" r="10" />
+            <polyline points="12 6 12 12 16 14" />
+          </svg>
+          <span>Lunes a viernes 08:00 - 18:00</span>
         </div>
       </div>
       <div className="mt-6">
-        <div className="w-full flex justify-center">
-        <iframe
-          title="C.M.S Laboral Location"
-          src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d13127.413529264675!2d-58.3889906!3d-34.6037345!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bccadc3ef576f5%3A0x821c1bd129e5e882!2sAvenida%20Corrientes%20587%2C%20C1042AAN%20CABA%2C%20Argentina!5e0!3m2!1sen!2sar!4v1721398328291!5m2!1sen!2sar"
-          width="100%"
-          height="250"
-          style={{ border: 0 }} 
-          allowFullScreen={false}
-          loading="lazy"
-          referrerPolicy="no-referrer-when-downgrade"
-        />
-        </div>
+       <div className="w-full flex justify-center">
+  <iframe
+    title="C.M.S Laboral Location"
+    src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3284.0413945276646!2d-58.37680302311869!3d-34.60311475746838!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x95bccacd015272c7%3A0xa131d5feebec7fa3!2sAv.%20Corrientes%20531%2C%20C1043AAF%20Cdad.%20Aut%C3%B3noma%20de%20Buenos%20Aires%2C%20Argentina!5e0!3m2!1sen!2ses!4v1753547710600!5m2!1sen!2ses"
+    width="100%"
+    height="300"
+    style={{ border: 0 }}
+    allowFullScreen={true}
+    loading="lazy"
+    referrerPolicy="no-referrer-when-downgrade"
+  />
+</div>
       </div>
     </CardContent>
   );
